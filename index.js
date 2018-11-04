03b9350a8c98cb668d5b00c2d165f6f8377815cc
Timothy Meyer <timothymeyer@Timothys-MacBook-Pro.local>
2018-11-04 15:30:34 -0600

    added parentheses

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(__dirname)
+  .use(express.static(__dirname))
   .get('/', (req, res) => res.render('index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
